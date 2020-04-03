958b0e52689f8e180e17d72bac7e968df81fa424
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-04-03 15:23:02 +0300

    The components are divided into modules, connected them to a single file and configured the assembly using webpack

---
+++ src/components/load-more-button.js
@@ -0,0 +1,6 @@
+// Возвращает разметку для кнопки загрузить еще
+export const createLoadMoreButtonTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
