43da3a7145fa11a98e89adb1b5d965c4700f7457
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-01 12:48:26 +0300

    Added code refactoring

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ const dateFrom = (() => {
   return d;
 })();
 
-
 const api = new API(END_POINT, AUTHORIZATION);
 const store = new Store(STORE_NAME, window.localStorage);
 const apiWithProvider = new Provider(api, store);
